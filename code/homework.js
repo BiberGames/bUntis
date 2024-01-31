f6e0648924229900394461be464779f0e47c7e0e
ben <whateveritwars@web.de>
2024-01-31 10:01:03 +0100

    More homework rework

--- code/homework.js
+++ code/homework.js
@@ -1,26 +1,24 @@
 const show = function(homeWorkTable, homeWorkData) {
-    //console.log(homeWork);
+    //console.log(homeWorkData);
     for(let i = 0; i < homeWorkData.homeworks.length; i++) {
         var row = homeWorkTable.insertRow();
         var cellSubject = row.insertCell();
-        var cellFrom = row.insertCell();
-        var cellTo = row.insertCell();
+        var cellTimeSpan = row.insertCell();
         var cellHomeworkText = row.insertCell();
         var cellDone = row.insertCell();
 
         // if homework is marked compleated
         if(homeWorkData.homeworks[i].completed == true) {
             cellSubject.style.backgroundColor += 'green';
-            cellFrom.style.backgroundColor += 'green';
-            cellTo.style.backgroundColor += 'green';
+            cellTimeSpan.style.backgroundColor += 'green';
             cellHomeworkText.style.backgroundColor += 'green';
+            //cellDone.innerHTML = '<input type="checkbox" onclick="return false" checked/>';
         }
 
         cellSubject.innerHTML = getSubjectFromHomeWork(homeWorkData.homeworks[i].lessonId, homeWorkData.lessons);
-        cellFrom.innerHTML = utils.convertUntisDate(homeWorkData.homeworks[i].date);
-        cellTo.innerHTML = utils.convertUntisDate(homeWorkData.homeworks[i].dueDate);
+        cellTimeSpan.innerHTML = utils.convertUntisDate(homeWorkData.homeworks[i].date) + ' to ' +utils.convertUntisDate(homeWorkData.homeworks[i].dueDate);
         cellHomeworkText.innerHTML = homeWorkData.homeworks[i].text;
-        cellDone.innerHTML = '<input type="checkbox">';
+        cellDone.innerHTML = '<input type="checkbox" onclick="return false"/>';
     }
 }
 
