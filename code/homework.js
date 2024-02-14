e339ac0961b6c6bc179e87803c200b045b5fcc42
ben <whateveritwars@web.de>
2024-02-14 17:50:58 +0100

    Hiding homework from table when marked completed

--- code/homework.js
+++ code/homework.js
@@ -1,19 +1,14 @@
 const show = function(homeWorkTable, homeWorkData) {
     //console.log(homeWorkData);
     for(let i = 0; i < homeWorkData.homeworks.length; i++) {
+	if(homeWorkData.homeworks[i].completed == true) {
+            return;
+        }
+	
         var row = homeWorkTable.insertRow();
         var cellSubject = row.insertCell();
         var cellTimeSpan = row.insertCell();
         var cellHomeworkText = row.insertCell();
-        var cellDone = row.insertCell();
-
-        // if homework is marked compleated
-        if(homeWorkData.homeworks[i].completed == true) {
-            cellDone.innerHTML = 'Completed';
-        }
-        else {
-            cellDone.innerHTML = 'Pending';
-        }
 
         cellSubject.innerHTML = getSubjectFromHomeWork(homeWorkData.homeworks[i].lessonId, homeWorkData.lessons);
         cellTimeSpan.innerHTML = utils.convertUntisDate(homeWorkData.homeworks[i].date) + ' to ' +utils.convertUntisDate(homeWorkData.homeworks[i].dueDate);
